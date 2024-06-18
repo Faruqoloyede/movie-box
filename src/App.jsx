3b118ffc8c3f738a597278e44eb813fdb3ca6e5d
Faruqoloyede <oloyedefaruq2@gmail.com>
2024-06-18 04:52:58 -0700

    integreated the OMDb api

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
-import search from "./assets/search.png";
-import cook from "./assets/cooking.png";
+import React, { useEffect, useState } from 'react';
+import Search from './components/Search';
+import MovieList from './components/MovieList';
 
 function App() {
-  return(
+ const [movies, setMovies] = useState([]);
+ const [isloading, setIsloading] = useState(true);
+ const [term, setTerm] = useState("");
+
+ const getMovies = async ()=>{
+    try {
+      const url = `http://www.omdbapi.com/?s=star wars&apikey=7fe4c5c5`;
+      const res = await fetch(url);
+      const resJson = await res.json();
+      // console.log(resJson);
+      setMovies(resJson.Search)
+      setIsloading(false)
+    } catch(error) {
+      console.log(error);
+      setIsloading(false);
+    }
+ };
+
+ useEffect(()=>{
+  getMovies();
+ }, [term])
+
+  return (
     <div className="flex flex-col justify-center items-center px-16">
       <h1 className="text-[3rem] text-white">MovieBox</h1>
-      <div className="flex items-center justify-center bg-black w-[71%] mt-5 py-3 px-4 rounded-[30px] search ">
-        <input type="search" className="bg-[#1f2123] flex-1 pr-3 text-[#a1a1a1] font-medium font-Raleway search_input" />
-        <img src={search} alt="" className="w-[30px] h-[30px] cursor-pointer" />
-      </div>
-      <div className="mt-4 flex flex-col items-center justify-center">
-        <img src={cook} alt="" className="w-[250px]" />
-        <h4 className="mt-5 text-white font-bold text-[30px]">opps! 😌</h4>
-        <p className="text-white text-[18px]">😜 sorry chief faruq is currently cooking this web app pls come back later 😜</p>
-      </div>
+      <Search searchText={(text)=> setTerm(text)} />
+      {isloading ? <h1 className='text-white text-3xl text-center'>Loading....</h1> : <div className='grid grid-rows sm:grid-cols-3 gap-5'>
+        {movies.map((movie)=>(
+          <div>
+            <MovieList key={movie.imdbID} movie={movie} />
+          </div>
+        ))}
+      </div>}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-import { logo, hero } from '../assets'
 
-const MovieList = () => {
+
+const MovieList = ({ movie }) => {
   return (
-    <div></div>
+    <div>
+        <div className='max-w-sm rounded-[12px] overflow-wrap cursor-pointer movies mt-7'>
+      <img src={movie.Poster} alt="poster"/>
+      <div className='max-w-[300px] px-3 py-4 bg-black overflow-wrap'>
+        <div className='font-normal text-[#9ef3fc] text-[18px] mb-2 font-roboto'>
+            Title: {movie.Title} <br />
+        </div>
+        <ul className='list-none text-[#a1a1a1] font-roboto'>
+            <li>
+                <span>Type: {movie.Type}</span>
+            </li>
+            <li>
+                <span>Year: {movie.Year}</span>
+            </li>
+        </ul>
+      </div>
+    </div>
+
+    
+    </div>
   )
 }
 
